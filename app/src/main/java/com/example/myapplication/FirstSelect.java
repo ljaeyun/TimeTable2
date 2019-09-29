6373b9708a02a9f62cbc4cbec3824e5d60958767
jaeyun <jae9864@naver.com>
2019-09-29 17:09:29 +0900

    전공개수선택 삭제

--- app/src/main/java/com/example/myapplication/FirstSelect.java
+++ app/src/main/java/com/example/myapplication/FirstSelect.java
@@ -15,7 +15,7 @@ public class FirstSelect extends AppCompatActivity {
     String spw;
 
     CheckBox chksubMajor, chkdoubleMajor;
-    Spinner spinnerMajor1, spinnerMajor2, selMajorCount, selDoubleCount, selMajorSpinner, selMinorSpinner;
+    //Spinner spinnerMajor1, spinnerMajor2, selMajorCount, selDoubleCount, selMajorSpinner, selMinorSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,58 +28,58 @@ public class FirstSelect extends AppCompatActivity {
         syear = intent1.getIntExtra("studentYear", 1);
         smajor = intent1.getIntExtra("studentMajor", 1);
 
-        selMajorSpinner = (Spinner) findViewById(R.id.MajorNum);
-        selMinorSpinner = (Spinner) findViewById(R.id.MinorNum);
+        // selMajorSpinner = (Spinner) findViewById(R.id.MajorNum);
+        // selMinorSpinner = (Spinner) findViewById(R.id.MinorNum);
 
-        selMajorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                majorNum = position;
-            }
+//        selMajorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                majorNum = position;
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        selMinorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                minorNum = position;
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+        //selMinorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                minorNum = position;
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
 
         chksubMajor = (CheckBox) findViewById(R.id.subMajor);
         chkdoubleMajor = (CheckBox) findViewById(R.id.doubleMajor);
 
-        spinnerMajor1 = (Spinner) findViewById(R.id.spinnerMajor1);
-        spinnerMajor1.setVisibility(View.INVISIBLE);
+        // spinnerMajor1 = (Spinner) findViewById(R.id.spinnerMajor1);
+        // spinnerMajor1.setVisibility(View.INVISIBLE);
 
-        spinnerMajor2 = (Spinner) findViewById(R.id.spinnerMajor2);
-        spinnerMajor2.setVisibility(View.INVISIBLE);
+        // spinnerMajor2 = (Spinner) findViewById(R.id.spinnerMajor2);
+        // spinnerMajor2.setVisibility(View.INVISIBLE);
 
-        selMajorCount = (Spinner) findViewById(R.id.subMajorNum);
-        selMajorCount.setVisibility(View.INVISIBLE);
+        // selMajorCount = (Spinner) findViewById(R.id.subMajorNum);
+        // selMajorCount.setVisibility(View.INVISIBLE);
 
-        selDoubleCount = (Spinner) findViewById(R.id.doubleMajorNum);
-        selDoubleCount.setVisibility(View.INVISIBLE);
+        //  selDoubleCount = (Spinner) findViewById(R.id.doubleMajorNum);
+        // selDoubleCount.setVisibility(View.INVISIBLE);
 
         chksubMajor.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean arg1) {
                 if (chksubMajor.isChecked() == true)    // 나중에 애니메이션효과 추가1
                 {
-                    spinnerMajor1.setVisibility(View.VISIBLE);
-                    selMajorCount.setVisibility(View.VISIBLE);
+                    //  spinnerMajor1.setVisibility(View.VISIBLE);
+                    // selMajorCount.setVisibility(View.VISIBLE);
                 } else {
-                    spinnerMajor1.setVisibility(View.INVISIBLE);
-                    selMajorCount.setVisibility(View.INVISIBLE);
+                    //   spinnerMajor1.setVisibility(View.INVISIBLE);
+                    //  selMajorCount.setVisibility(View.INVISIBLE);
                 }
             }
         });
@@ -89,62 +89,62 @@ public class FirstSelect extends AppCompatActivity {
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (chkdoubleMajor.isChecked() == true) // 나중에 애니메이션효과 추가2
                 {
-                    spinnerMajor2.setVisibility(View.VISIBLE);
-                    selDoubleCount.setVisibility(View.VISIBLE);
+                    //   spinnerMajor2.setVisibility(View.VISIBLE);
+                    //  selDoubleCount.setVisibility(View.VISIBLE);
                 } else {
-                    spinnerMajor2.setVisibility(View.INVISIBLE);
-                    selDoubleCount.setVisibility(View.INVISIBLE);
+                    //   spinnerMajor2.setVisibility(View.INVISIBLE);
+                    //   selDoubleCount.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
-        spinnerMajor1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                subMajor = position;//부전공
-            }
+//        spinnerMajor1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                subMajor = position;//부전공
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
+//        spinnerMajor2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                doubleMajor = position;//복수전공
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
 
-            }
-        });
-
-        spinnerMajor2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                doubleMajor = position;//복수전공
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        selMajorCount.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                subMajorNum = position;//부전공개수
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        selDoubleCount.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                doubleMajorNum = position;//복수전공개수
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
+//        selMajorCount.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                subMajorNum = position;//부전공개수
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
+//
+//        selDoubleCount.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                doubleMajorNum = position;//복수전공개수
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//            }
+//        });
     }
 
     public void onClick(View view) {
